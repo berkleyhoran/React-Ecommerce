d31cef45980fbfde43c09172de3a3c141da99afa
Berkley Horan <horans11@gmail.com>
2019-04-15 21:18:27 -0600

    Updates

---
+++ precache-manifest.22e00154ebe43e9e7722336634dd8a23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b62a585543d74765339",
+    "url": "/React-Ecommerce/static/css/main.d2158801.chunk.css"
+  },
+  {
+    "revision": "0b62a585543d74765339",
+    "url": "/React-Ecommerce/static/js/main.8e5ba5db.chunk.js"
+  },
+  {
+    "revision": "1e6f0726f68087019158",
+    "url": "/React-Ecommerce/static/js/runtime~main.d3b74b69.js"
+  },
+  {
+    "revision": "1c74df3103f2fb898c9a",
+    "url": "/React-Ecommerce/static/js/2.310282eb.chunk.js"
+  },
+  {
+    "revision": "181b302948ec0b9fcb6d079c51c0787f",
+    "url": "/React-Ecommerce/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.4ef98737af40e2e78074e171a0c4257a.js"
+  "/React-Ecommerce/precache-manifest.22e00154ebe43e9e7722336634dd8a23.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/myapp/index.html", {
+workbox.routing.registerNavigationRoute("/React-Ecommerce/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
